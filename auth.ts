46973eab408d3de832c09136bea426dbfba8b459
borishitz <borishitz@gmail.com>
2025-06-13 07:10:37 +0100

    just push now

--- auth.ts
+++ auth.ts
@@ -1,42 +1,35 @@
 import NextAuth from "next-auth";
 import { PrismaAdapter } from "@auth/prisma-adapter";
-import { getUserById } from "@/data/user"
-import {db} from "@/lib/db";
+import { getUserById } from "@/data/user";
+import { db } from "@/lib/db";
 import authConfig from "./auth.config";
 import { UserRole } from "@prisma/client";
 
-export const {
-    handlers: { GET, POST },
-    auth,
-    signIn, 
-    signOut 
-} = NextAuth({
-        callbacks: {
-            async session(params) {
-                const { session } = params;
+const handler = NextAuth({
+  adapter: PrismaAdapter(db),
+  session: { strategy: "jwt" },
+  ...authConfig,
+  callbacks: {
+    async session({ token, session }) {
+      if (token && session.user) {
+        session.user.id = token.sub!;
+        session.user.role = token.role as UserRole;
+        session.user.customField = "go get me the inform";
+      }
+      return session;
+    },
 
-                if ('token' in params && params.token && session.user) {
-                session.user.id = params.token.sub!;
-                session.user.role = params.token.role as UserRole;
-                session.user.customField = "go get me the inform";
-                }
+    async jwt({ token }) {
+      if (!token.sub) return token;
 
-                return session;
-            },
+      const existingUser = await getUserById(token.sub);
+      if (!existingUser) return token;
 
-            async jwt({ token }) {
-                if (!token.sub) return token;
+      token.role = existingUser.role;
+      return token;
+    },
+  },
+});
 
-                const existingUser = await getUserById(token.sub);
-                if (!existingUser) return token;
-
-                token.role = existingUser.role;
-                return token;
-            }
-            },
-
-            adapter: PrismaAdapter(db),
-            session: { strategy: "jwt"},
-        ...authConfig,
-})
-
+export const { auth, signIn, signOut } = handler;
+export const { GET, POST } = handler.handlers;
